e0f4b242d83e1669b8c8a6d39c1e037d760b5b84
System User <sfriston@maxworkstation7494.office.maxeler.com>
2015-01-27 16:15:51 +0000

    * first checkin, runs on hardware 1024x768x60 with maxeleros 2014.2

---
+++ CPUCode/MaxResolutionTestCpuCode.cpp
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Maxfiles.h"
+#include "MaxSLiCInterface.h"
+
+int main(void)
+{
+
+	printf("Running on DFE.\n");
+
+	max_file_t *maxfile = MaxResolutionTest_init();
+	max_engine_t *engine = max_load(maxfile, "*");
+	max_actions_t* act = max_actions_init(maxfile, NULL);
+
+	max_set_uint64t(act,"MaxVideoSignalKernel","HSyncPolarity",1);
+	max_set_uint64t(act,"MaxVideoSignalKernel","VSyncPolarity",1);
+
+	max_run(engine, act);
+
+	printf("Press n key to exit...");
+
+	while(getchar() != 'n')
+	{
+
+	}
+
+	max_unload(engine);
+
+	printf("Done.\n");
+	return 0;
+}

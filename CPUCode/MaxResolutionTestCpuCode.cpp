7bf5060b785a54deb3362dae3a47d596d51675a1
System User <sfriston@nxws24.office.maxeler.com>
2015-05-27 19:05:28 +0100

    * modified manager to work with new way of getting video stream

--- CPUCode/MaxResolutionTestCpuCode.cpp
+++ CPUCode/MaxResolutionTestCpuCode.cpp
@@ -8,11 +8,14 @@
 int main(void)
 {
 
-	printf("Running on DFE.\n");
+	printf("Running on DFE. v0.5\n");
 
 	max_file_t *maxfile = MaxResolutionTest_init();
+	max_set_max_runnable_timing_score(maxfile, 16123321);
+
 	max_engine_t *engine = max_load(maxfile, "*");
 	max_actions_t* act = max_actions_init(maxfile, NULL);
+
 
 	max_set_uint64t(act,"MaxVideoSignalKernel","HSyncPolarity",1);
 	max_set_uint64t(act,"MaxVideoSignalKernel","VSyncPolarity",1);
